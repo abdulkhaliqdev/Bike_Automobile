3b47aa50524ce1fa792b50b2c733d4f240a6d0e9
AK-Devil <abdulkhaliql1f18bscs0159@gmail.com>
2021-01-29 04:23:57 +0500

    Add footer, header and logo

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
-  include './HTML/header.html';
-  include './HTML/footer.html';
+  $connection=mysqli_connect("localhost","root","studentData","mysql") or die("Could not Connect".mysqli_connect_error());
+  if($connection){
+    echo "Connection is Sucessful";
+  }
+  mysqli_close($connection);
+  // include './HTML/header.html';
+  // include './HTML/footer.html';
 ?>
